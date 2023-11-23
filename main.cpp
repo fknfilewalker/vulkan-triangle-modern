40fbffcb6731d0ae1df132af89011fbf4f07f93e
Lukas Lipp <llipp@cg.tuwien.ac.at>
2023-11-23 11:23:29 +0100

    instance creation

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
-#include "vulkan/vulkan_raii.hpp"
+#include <vulkan/vulkan_raii.hpp>
+#include <vector>
+
+class App {
+    
+};
 
 int main(int argc, char *argv[])
 {
-    return 1;
+    vk::raii::Context context;
+    
+    vk::ApplicationInfo applicationInfo;
+    applicationInfo.apiVersion = VK_VERSION_1_3;
+    
+    std::vector<const char*> extensions;
+#ifdef __APPLE__
+    extensions.push_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+#endif
+    
+    vk::InstanceCreateInfo instanceCreateInfo;
+    instanceCreateInfo.setPApplicationInfo(&applicationInfo);
+    instanceCreateInfo.setPEnabledExtensionNames(extensions);
+#ifdef __APPLE__
+    instanceCreateInfo.setFlags(vk::InstanceCreateFlagBits::eEnumeratePortabilityKHR);
+#endif
+    vk::raii::Instance instance( context, instanceCreateInfo);
+    
+    return 0;
 }
