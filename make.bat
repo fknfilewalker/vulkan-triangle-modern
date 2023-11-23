0c10274aaa51ca9f60a684bbc4e2817e92cd614f
Lukas Lipp <llipp@cg.tuwien.ac.at>
2023-11-23 13:25:40 +0100

    windows make bat

---
+++ make.bat
@@ -0,0 +1,29 @@
+@ECHO OFF
+set PROJECT_DIR=_project
+set INSTALL_DIR=_bin
+
+if "%~1"=="" goto BLANK
+if "%~1"=="install" goto install
+if "%~1"=="test" goto test
+if "%~1"=="clean" goto CLEAN
+@ECHO ON
+
+:BLANK
+cmake -H. -B %PROJECT_DIR% -A "x64"
+GOTO DONE
+
+:INSTALL
+:: Windows
+echo "Note: Run make.bat clean to delete the output directories and then run make.bat install to run a clean build!"
+cmake -H. -B %PROJECT_DIR% -A "x64" -DTOOLCHAIN_ACTIVE=ON
+cmake --build %PROJECT_DIR% --parallel 24 --config Debug --target install
+cmake --build %PROJECT_DIR% --parallel 24 --config Release --target install
+GOTO DONE
+
+:CLEAN
+rmdir /Q /S %PROJECT_DIR% 2>NUL
+rmdir /Q /S %LINUX_PROJECT_DIR% 2>NUL
+rmdir /Q /S %INSTALL_DIR% 2>NUL
+GOTO DONE
+
+:DONE
